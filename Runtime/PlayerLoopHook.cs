5def2c791c9f3eefd51927d05d80a3ff7259b105
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-23 10:16:16 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/PlayerLoopHook.cs
+++ Runtime/PlayerLoopHook.cs
@@ -2,6 +2,12 @@ using UnityEngine.LowLevel;
 
 namespace UnityEssentials
 {
+    /// <summary>
+    /// Provides utility methods for adding and removing custom update delegates to the Unity player loop system.
+    /// </summary>
+    /// <remarks>The <see cref="PlayerLoopHook"/> class allows developers to inject or remove custom update
+    /// logic into specific subsystems of the Unity player loop. This can be useful for extending or modifying the
+    /// behavior of the game loop without directly modifying Unity's internal systems.</remarks>
     public static class PlayerLoopHook
     {
         public static void Add<T>(PlayerLoopSystem.UpdateFunction updateDelegate) where T : struct
